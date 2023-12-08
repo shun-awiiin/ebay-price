39c9d82168bc7aab17c65b1c2e19aba1d00d67d9
awiiin <kikuchi_shun@awiiin.com>
2023-12-08 10:31:04 +0900

    watch保存・抽出

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ import logging
 from google.cloud import datastore
 import os
 import secrets
+import csv
+
 
 secret_key = secrets.token_hex(16)
 
@@ -146,9 +148,48 @@ def ebay_connect():
         return render_template("ebay-connect.html")
 
 
+def read_category_ids_from_csv(file_path):
+    """
+    CSVファイルからカテゴリーIDを読み込む関数。
+    """
+    category_ids = []
+    with open(file_path, mode="r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            category_ids.append(row["CategoryId"])
+    return category_ids
+
+
+def fetch_data_from_datastore(client, category_id):
+    """
+    Datastoreから特定のカテゴリIDに基づくデータを取得する関数。
+    """
+    query = client.query(kind=f"Watch_{category_id}")
+    return list(query.fetch())
+
+
 @app.route("/layout-static")
 def layout_static():
-    return render_template("layout-static.html")
+    client = datastore.Client()  # Google Cloud Datastoreクライアントの初期化
+    category_ids = read_category_ids_from_csv("categories.csv")
+
+    page = request.args.get("page", 1, type=int)
+    per_page = 10  # 1ページあたりのアイテム数
+
+    items = []
+    for category_id in category_ids:
+        items.extend(fetch_data_from_datastore(client, category_id))
+
+    total_items = len(items)
+    total_pages = total_items // per_page + (1 if total_items % per_page else 0)
+    start = (page - 1) * per_page
+    end = start + per_page
+
+    paginated_items = items[start:end]
+
+    return render_template(
+        "layout-static.html", items=paginated_items, page=page, total_pages=total_pages
+    )
 
 
 @app.route("/layout-sidenav-light")
