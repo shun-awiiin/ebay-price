70da06c621cb0ac0f79ddeb6c67d866dec374ae6
awiiin <kikuchi_shun@awiiin.com>
2023-12-15 11:10:29 +0900

    home画面修正

--- static/js/edit_script.js
+++ static/js/edit_script.js
@@ -1,19 +1,57 @@
 $(document).ready(function() {
-    $('#editTitle, #editDescription, #editPrice').on('input', function() {
-        var previewId = '#preview' + this.id.substring(4);
-        $(previewId).text($(this).val());
+    // リアルタイムプレビュー機能
+    $('#editTitle').on('input', function() {
+        $('#previewTitle').text($(this).val());
     });
-
+    $('#editDescription').on('input', function() {
+        $('#previewDescription').text($(this).val());
+    });
+    $('#editPrice').on('input', function() {
+        $('#previewPrice').text('$' + $(this).val());
+    });
     $('#editImage').on('change', function() {
         var file = this.files[0];
         if (file) {
             var reader = new FileReader();
             reader.onload = function(e) {
-                $('#previewImage').attr('src', e.target.result).show();
+                $('#previewImage').attr('src', e.target.result);
             };
             reader.readAsDataURL(file);
-        } else {
-            $('#previewImage').hide();
         }
     });
+
+    // 保存ボタンの処理
+    $('#saveChanges').click(function() {
+        // Ajaxを使用して変更を保存する処理を記述する
+        // ...
+    });
+});
+
+$(document).ready(function() {
+    $('.auto-resize').each(function() {
+        this.style.height = (this.scrollHeight) + 'px';
+    }).on('input', function() {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+});
+
+$(document).ready(function() {
+    var $editTitle = $('#editTitle');
+    var $titleCount = $('#titleCount');
+
+    // 初期ロード時の文字数を設定
+    updateTitleCount();
+
+    $editTitle.on('input', function() {
+        updateTitleCount();
+        // auto-resizeロジック
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+
+    function updateTitleCount() {
+        var currentLength = $editTitle.val().length;
+        $titleCount.text(currentLength + ' / 80');
+    }
 });
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -24,3 +24,65 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+
+function startTutorial() {
+    introJs().start();
+}
+
+
+// YouTube APIを使用してデータを取得
+function fetchYouTubeVideos() {
+    const apiKey = 'AIzaSyBzhjIJonY_CUu2WpptrlbYYsAOF0_dyE8';
+    const channelId = 'UCpbAXFvLXA7a4vItLq6h0GQ';
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=20`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayYouTubeVideos(data.items));
+}
+
+function displayYouTubeVideos(videos) {
+    const container = document.getElementById('youtube-container');
+    videos.slice(0, 5).forEach(video => {
+        const videoElement = document.createElement('div');
+        videoElement.className = 'video-card';
+        videoElement.innerHTML = `
+            <img src="${video.snippet.thumbnails.medium.url}" alt="${video.snippet.description}" class="video-thumbnail">
+            <h3 class="title-small">${video.snippet.title}</h3>
+            <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank"class=" read-more-link">ビデオを見る</a>
+        `;
+        container.appendChild(videoElement);
+    });
+}
+
+function fetchBlogPosts() {
+    const blogApiUrl = 'https://ebay-marketing-tool.com/wp-json/wp/v2/posts';
+    fetch(blogApiUrl)
+        .then(response => response.json())
+        .then(posts => displayBlogPosts(posts));
+}
+
+function displayBlogPosts(posts) {
+    const container = document.getElementById('blog-container');
+    posts.slice(0, 5).forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.className = 'post-card';
+
+        // サムネイルのURLを取得
+        const thumbnailUrl = post._embedded && post._embedded['wp:featuredmedia']
+            ? post._embedded['wp:featuredmedia'][0].source_url
+            : 'https://ewmqoegsx4q.exactdn.com/wp-content/uploads/2023/08/EXPO3.png?strip=all&lossy=1&ssl=1'; // デフォルトのサムネイルURL
+
+        postElement.innerHTML = `
+            <img src="${thumbnailUrl}" alt="${post.title.rendered}" class="post-thumbnail">
+            <h3 class="title-small">${post.title.rendered}</h3>
+            <a href="${post.link}" target="_blank" class="read-more-link">記事を読む</a>
+        `;
+        container.appendChild(postElement);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchBlogPosts();
+    fetchYouTubeVideos(); // この行を追加
+});
